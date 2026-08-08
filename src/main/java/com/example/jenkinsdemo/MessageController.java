63566617c2d2cbd0f4012f891065fc40e426511a
Nematullah19 <nematullah.cis.1377@gmail.com>
2026-08-08 15:07:24 +0430

    Initial sping boot Jenkins demo project

---
+++ src/main/java/com/example/jenkinsdemo/MessageController.java
@@ -0,0 +1,18 @@
+
+
+
+package com.example.jenkinsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/api/message")
+    public String getMessage() {
+        return "Hello from Spring Boot - Deployed by Jenkins!";
+    }
+}
+
+
