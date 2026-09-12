8959009b13ec8ea890836b122ae55789a294994a
Nematullah19 <nematullah.cis.1377@gmail.com>
2026-09-12 08:39:19 +0430

    Update demo application  version from 2.8 to 2.9

--- src/test/java/com/example/jenkinsdemo/FeatureControllerTest.java
+++ src/test/java/com/example/jenkinsdemo/FeatureControllerTest.java
@@ -31,6 +31,6 @@ class FeatureControllerTest {
                 controller.status();
 
         assertEquals("UP", response.get("status"));
-        assertEquals("2.8", response.get("version"));
+        assertEquals("2.9", response.get("version"));
     }
 }
