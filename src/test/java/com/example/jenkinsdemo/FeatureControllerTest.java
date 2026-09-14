cf1eaf869244532797bb8d6ec996533b51576f60
Administrator <gitlab_admin_ec8045@example.com>
2026-09-14 09:49:52 +0430

    changes application from verison 2.12 to 2.13

--- src/test/java/com/example/jenkinsdemo/FeatureControllerTest.java
+++ src/test/java/com/example/jenkinsdemo/FeatureControllerTest.java
@@ -31,6 +31,6 @@ class FeatureControllerTest {
                 controller.status();
 
         assertEquals("UP", response.get("status"));
-        assertEquals("2.12", response.get("version"));
+        assertEquals("2.13", response.get("version"));
     }
 }
